5607eb446c0c81e2068a6685db57917a21462799
lee sua <s2332@e-mirim.hs.kr>
2025-05-27 14:50:23 +0900

    Box 클래스 getter, setter 구현

--- Box.cs
+++ Box.cs
@@ -25,6 +25,20 @@ namespace PropExample
             }
         }
 
+        public int getWidth() { return this.width; }
+        public int getHeight() { return this.height; }
+        public void setWidth()
+        {
+            if(width > 0) { this.width = width; }
+            else { Console.WriteLine("너비는 자연수로 입력해주세요"); }
+        }
+
+        public void setHeight()
+        {
+            if (height > 0) { this.height = height; }
+            else { Console.WriteLine("높이는 자연수로 입력해주세요"); }
+        }
+
         public int Area()
         {
             return this.width * this.height;
