13d90fbda429f547a393e8528aaa318af3394c03
lee sua <s2332@e-mirim.hs.kr>
2025-05-29 09:05:19 +0900

    생성자 - 팩토리 메서드 패턴(생성자에 private를 사용하는 경우)

--- Program.cs
+++ Program.cs
@@ -18,6 +18,13 @@ namespace PropExample
             //Program p = new Program();
             //p.classVar = -3;
 
+            //생성자
+            //기본생성자 - 생성자를 하나라도 정의하면 기본생성자를 따로 작성해줘야함
+            //Product p = new Product();
+
+            //팩토리 메서드 패턴 (생성자에 private를 사용하는 경우)
+            Product p = Product.GetInstance("아아", 1500);
+
             //property 실습
             Box box1 = new Box(100, 200);
             box1.Width = -3;
